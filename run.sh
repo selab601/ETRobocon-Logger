77ecea709e5a6e86dbe46f9033ccae11d405b435
Tasuku Tozawa <15nm722x@vc.ibaraki.ac.jp>
2016-09-13 18:43:21 +0900

    run.shはいらないと思うので削除

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -z $1 ]; then
-    echo "引数に /dev/tty.[名前]SerialPortProfile を指定してください"
-else
-    echo "終了するには Ctrl-C を押すか Command-Q で終了してください"
-    node_modules/electron-prebuilt/dist/Electron.app/Contents/MacOS/Electron . $1
-fi
