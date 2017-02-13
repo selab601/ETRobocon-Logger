c4f31e738c99e402126f8da75620e8ba269ecd36
tasuwo <kamuhata.you@gmail.com>
2017-02-13 23:01:21 +0900

    テスト用の fake モジュールを用意する

--- main.js
+++ main.js
@@ -5,36 +5,26 @@
 const {app, BrowserWindow} = require('electron'),
       path = require('path'),
       url = require('url'),
-      // main プロセス側の IPC 用モジュール
-      ipcMain = require('electron').ipcMain,
-      // Bluetooth デバイスの検索&通信を行うためのモジュール
-      btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort(),
-      EventEmitter = require('events').EventEmitter;
+      is_fake = true,
+      deviceConnector = is_fake ? require("./lib/fakeConnector.js") : require("./lib/deviceConnector.js");
 
-    // メインウインドウをグローバル変数として保持しておく
-    // これがないと，JSのGCにウインドウを殺されてしまう
-var mainWindow,
-    file = require('fs'),
-    logFilePath,
-    logFileName,
-    didFinishLoaded = false;
-
-// ログファイルで日付を使用するためのモジュール
-require('date-utils');
+// メインウインドウをグローバル変数として保持しておく
+// これがないと，JSのGCにウインドウを殺されてしまう
+var mainWindow, dc;
 
 /******** アプリケーションのイベントハンドラ登録 ********/
 
 // 起準備動時の処理
 app.on('ready', function () {
   createWindow();
-  updateLogFileName();
+  dc.updateLogFileName();
 });
 
 // 終了時の処理
 app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit();
-    btSerial.close();
+    dc.closeBt();
   }
 });
 
@@ -48,108 +38,14 @@ app.on('activate', function() {
 /****** アプリケーションのイベントハンドラ登録終了 ******/
 
 
-/********* プロセス間通信のイベントハンドラ登録 *********/
-
-var ipc = require('electron').ipcMain;
-
-ipc.on('updateDevices', (event, arg) => {
-  console.log("Starting find devices...");
-  var devices = [];
-
-  btSerial.on('found', function(address, name) {
-    console.log(name);
-    console.log(address);
-    devices.push({name: name, address: address});
-  });
-
-  var async = asyncFunc();
-  async.on('done', function() {
-    btSerial.close();
-
-    event.sender.send('updateDevicesFailed', {
-      title: "Finish",
-      body: devices.length + " devices were found"
-    });
-
-    for (var i=0; i<devices.length; i++) {
-      mainWindow.webContents.send('updateDevicesComplete', devices[i]);
-    }
-  });
-
-  btSerial.inquire();
-});
-
-ipc.on('connectDevice', (event, arg) => {
-  console.log("Connecting...");
-  var address = arg;
-  btSerial.findSerialPortChannel(address, function(channel) {
-    btSerial.connect(address, channel, function() {
-      console.log('connected');
-
-      event.sender.send('connectDeviceComplete', {
-        title: "Cnnected!",
-        body: "Successfully connected!!"
-      });
-
-      btSerial.on('data', function(buffer) {
-        mainWindow.webContents.send('ReceiveDataFromBTDevice', buffer);
-        file.appendFile(logFilePath, buffer, 'utf8');
-      });
-
-    }, function () {
-      event.sender.send('connectDeviceFailed', {
-        title: "Cannot Connect",
-        body: "Selected device was found, but failed to connect. Please try again"
-      });
-      console.log('cannot connect');
-    });
-
-  }, function() {
-    event.sender.send('connectDeviceFailed', {
-      title: "Found nothing",
-      body: "Selected device was not found"
-    });
-    console.log('found nothing');
-  });
-});
-
-ipc.on('disconnectDevice', (event, arg) => {
-  console.log("Disconnected");
-  btSerial.close();
-  btSerial = new BluetoothSerialPort.BluetoothSerialPort();
-
-  // 受信が続いていると新しくファイルを作ってしまうので，切断後ちょっとしてからファイル名を更新する
-  var async = asyncFunc();
-  async.on('done', function() {
-    // ログファイル名をリネーム
-    var path = app.getAppPath() + '/log/';
-    // TODO: 文字列が空だったりすると失敗するので，エラー処理が必要
-    file.renameSync(path + logFileName, path + arg);
-    updateLogFileName();
-    event.sender.send('disconnectDeviceComplete', {
-      title: "Disconnected",
-      body: "This connection's data was saved in " + arg + "."
-    });
-  });
-});
-
-/******* プロセス間通信のイベントハンドラ登録終了 *******/
-
-
 /******* 各種関数定義 *******/
-
-function asyncFunc() {
-  var ev = new EventEmitter;
-  setTimeout(function () {
-    ev.emit('done');
-  }, 5000);
-  return ev;
-};
-
 // メインウインドウの生成
 function createWindow() {
   mainWindow = new BrowserWindow({width: 1024, height: 600});
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  console.log(mainWindow);
+  dc = new deviceConnector( mainWindow, app.getAppPath() );
 
   // ウインドウが閉じられた場合の処理
   mainWindow.on('closed', function() {
@@ -158,31 +54,7 @@ function createWindow() {
 
   // TODO: 以前接続したことのあるBTデバイスをどこかに保存しておく
 
-  // 接続済みのものを renderer プロセスに教える
-  btSerial.listPairedDevices(function(pairedDevices) {
-    console.log(pairedDevices);
-    pairedDevices.forEach(function(device) {
-      mainWindow.webContents.on('did-finish-load', function () {
-        mainWindow.webContents.send('updateDevicesComplete', {name: device.name, address: device.address});
-      });
-    });
-  });
+  dc.sendListPairedDevices();
 };
-
-function updateLogFileName() {
-  var date = new Date();
-  var formatted = date.toFormat("YYYY_MMDD_HH24MISS");
-  logFileName = formatted + '.json';
-  logFilePath = app.getAppPath() + '/log/' + logFileName;
-
-  if (didFinishLoaded == false) {
-    mainWindow.webContents.on('did-finish-load', function () {
-      didFinishLoaded = true;
-      mainWindow.webContents.send('LogFileName', logFileName);
-    });
-  } else {
-    mainWindow.webContents.send('LogFileName', logFileName);
-  }
-}
 
 /***** 各種関数定義終了 *****/
