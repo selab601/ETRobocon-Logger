30bc1ac703ef11abbb7f5254cf95d62bff15f3e8
tasuwo <kamuhata.you@gmail.com>
2017-02-15 02:09:05 +0900

    deviceDisconnector モジュールのレイアウト修正

--- src/js/deviceDisconnector.js
+++ src/js/deviceDisconnector.js
@@ -2,7 +2,7 @@ function deviceDisconnector () {
   this.configMap = {
     main_html : (function () {
       /*
-        <div class="device-connector-button danger" id="disconnect-btn">DISCONNECT</div>
+        <div class="device-disconnector-button" id="disconnect-btn">DISCONNECT</div>
       */}).toString().replace(/(\n)/g, '').split('*')[1]
   };
   this.stateMap = {
@@ -10,6 +10,7 @@ function deviceDisconnector () {
   };
   this.jqueryMap = {};
   this.getLogFileName = undefined;
+  this.callback = undefined;
 
   this.ipc = require('electron').ipcRenderer;
   this.$ = require('./model/lib/jquery-3.1.0.min.js');
@@ -26,7 +27,7 @@ deviceDisconnector.prototype.onDisconnectDevice = function () {
 deviceDisconnector.prototype.onDisconnectDeviceComplete = function ( ev, message ) {
   console.log(message);
   this.stateMap.isConnected = false;
-  this.jqueryMap.$disconnect_btn.removeClass('disabled');
+  this.callback();
 };
 
 /****************************/
@@ -40,16 +41,28 @@ deviceDisconnector.prototype.setJqueryMap = function () {
   };
 };
 
-deviceDisconnector.prototype.initModule = function ( $append_target ) {
+deviceDisconnector.prototype.initModule = function ( $append_target, getLogFileName, callback ) {
   this.stateMap.$append_target = $append_target;
   $append_target.append( this.configMap.main_html );
   this.setJqueryMap();
 
   this.getLogFileName = getLogFileName;
+  this.callback = callback;
 
   // イベントハンドラの登録
   this.jqueryMap.$disconnect_btn.bind( 'click', this.onDisconnectDevice.bind(this));
   this.ipc.on('disconnectDeviceComplete', this.onDisconnectDeviceComplete.bind(this));
+};
+
+deviceDisconnector.prototype.removeModule = function () {
+  this.jqueryMap.$disconnect_btn.remove();
+  this.jqueryMap = {};
+  this.stateMap = {
+    $append_target : undefined
+  };
+  this.jqueryMap = {};
+  this.getLogFileName = undefined;
+  this.callback = undefined;
 };
 
 module.exports = deviceDisconnector;
