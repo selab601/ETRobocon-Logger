5e8ed354c114b61c552cb217a18a7b7c263bcac9
tasuwo <kamuhata.you@gmail.com>
2017-02-16 02:02:32 +0900

    ログに記録する値の種類の定義を別ファイルに抽出

---
+++ src/js/config/logged_values.js
@@ -0,0 +1,20 @@
+exports.values = [
+  { id : "gyro",         label : "ジャイロセンサ値" },
+  { id : "touch",        label : "タッチセンサ値" },
+  { id : "sonar",        label : "ソナー値" },
+  { id : "brightness",   label : "輝度値" },
+  { id : "rgb_r",        label : "RGB(R値)" },
+  { id : "rgb_g",        label : "RGB(G値)" },
+  { id : "rgb_b",        label : "RGB(B値)" },
+  { id : "hsv_h",        label : "HSV(H値)" },
+  { id : "hsv_s",        label : "HSV(S値)" },
+  { id : "hsv_v",        label : "HSV(V値)" },
+  { id : "arm_count",    label : "前輪エンコーダ値" },
+  { id : "left_count",   label : "左輪エンコーダ値" },
+  { id : "right_count",  label : "右輪エンコーダ値" },
+  { id : "length",       label : "走行距離" },
+  { id : "angle",        label : "車体角度" },
+  { id : "coordinate_x", label : "自己位置X座標" },
+  { id : "coordinate_y", label : "自己位置Y座標" },
+  { id : "clock",        label : "時刻" }
+];
--- src/js/logRenderer.js
+++ src/js/logRenderer.js
@@ -41,26 +41,7 @@ function logRenderer() {
           <label></label>
         </div>
       */}).toString().replace(/(\n)/g, '').split('*')[1],
-    graph_value_map : [
-      { id : "gyro",         label : "ジャイロセンサ値" },
-      { id : "touch",        label : "タッチセンサ値" },
-      { id : "sonar",        label : "ソナー値" },
-      { id : "brightness",   label : "輝度値" },
-      { id : "rgb_r",        label : "RGB(R値)" },
-      { id : "rgb_g",        label : "RGB(G値)" },
-      { id : "rgb_b",        label : "RGB(B値)" },
-      { id : "hsv_h",        label : "HSV(H値)" },
-      { id : "hsv_s",        label : "HSV(S値)" },
-      { id : "hsv_v",        label : "HSV(V値)" },
-      { id : "arm_count",    label : "前輪エンコーダ値" },
-      { id : "left_count",   label : "左輪エンコーダ値" },
-      { id : "right_count",  label : "右輪エンコーダ値" },
-      { id : "length",       label : "走行距離" },
-      { id : "angle",        label : "車体角度" },
-      { id : "coordinate_x", label : "自己位置X座標" },
-      { id : "coordinate_y", label : "自己位置Y座標" },
-      { id : "clock",        label : "時刻" }
-    ]
+    graph_value_map : require('./config/logged_values.js').values
   };
   // 動的なプロパティ
   this.stateMap = {
