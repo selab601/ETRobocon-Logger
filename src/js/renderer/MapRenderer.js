867f830e7b34f4c9d99b23d30afdac28934af04a
tasuwo <kamuhata.you@gmail.com>
2017-02-23 23:49:11 +0900

    map renderer モジュールにおいて，map 設定の値の検証を追加

--- src/js/renderer/MapRenderer.js
+++ src/js/renderer/MapRenderer.js
@@ -37,24 +37,28 @@ MapRenderer.prototype.init = function ( $append_target, map_settings, onSelectDa
   this.setJqueryMap();
 
   // マップの初期化
-  // TODO: map_settings の値の検証．
   // TODO: 設定がされていない場合には警告を出す
-  this.imageViewer.init(this.stateMap.$append_target.find("#maprenderer-box"));
-  this.imageViewer.setImage(map_settings.image_path, map_settings.image_original_size);
+  if (
+    map_settings.image_path != ''
+      && map_settings.image_original_size != ''
+      && map_settings.start_point != ''
+  ) {
+    this.imageViewer.init(this.stateMap.$append_target.find("#maprenderer-box"));
+    this.imageViewer.setImage(map_settings.image_path, map_settings.image_original_size);
 
-  var cor = { x: map_settings.start_point.x, y: map_settings.start_point.y };
-  this.map = new Map(
-    "imageviewer-image-wrapper",
-    map_settings.image_original_size.width,
-    map_settings.image_original_size.height,
-    { x: cor.x, y: cor.y },
-    map_settings.draw_scale,
-    onSelectData,
-    map_settings.draw_rotate
-  );
-  this.map.init();
-
-  this.imageViewer.setOnScaleCompleteHandler( this.map.scale.bind(this.map) );
+    var cor = { x: map_settings.start_point.x, y: map_settings.start_point.y };
+    this.map = new Map(
+      "imageviewer-image-wrapper",
+      map_settings.image_original_size.width,
+      map_settings.image_original_size.height,
+      { x: cor.x, y: cor.y },
+      map_settings.draw_scale,
+      onSelectData,
+      map_settings.draw_rotate
+    );
+    this.map.init();
+    this.imageViewer.setOnScaleCompleteHandler( this.map.scale.bind(this.map) );
+  }
 };
 
 module.exports = MapRenderer;
