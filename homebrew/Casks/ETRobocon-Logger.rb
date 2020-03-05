f37d1f47f9a6489fd698e11d3dded452079919ac
motoki nagaoka <motoki.23ar@gmail.com>
2020-03-05 22:52:43 +0900

    homebrew用インストールスクリプトの追加

---
+++ homebrew/Casks/ETRobocon-Logger.rb
@@ -0,0 +1,10 @@
+cask 'ETRobocon-Logger' do
+  version 'v0.1.0'
+  sha256 '6a45beea9a6295ba40bba6915b7ec01dc17e3c1041fe6ca77201a7a687310315'
+
+  url 'https://github.com/selab601/ETRobocon-Logger/releases/download/v0.1.0/ETRobocon-Logger.app.zip'
+  name 'ETRobocon-Logger'
+  homepage 'https://github.com/selab601/ETRobocon-Logger'
+
+  app 'ETRobocon-Logger.app'
+end
---
+++ homebrew/README.md
@@ -0,0 +1,14 @@
+# homebrew-ETRobocon-Logger
+Homebrew-Cask で ETRobocon-Logger をインストールします
+
+## ロガーのインストール
+ETRobocon-Logger のリポジトリを追加
+``` sh
+brew tap selab601/ETRobocon-Logger
+```
+
+ETRobocon-Logger のインストール
+
+``` sh
+brew cask install ETRobocon-Logger
+```
